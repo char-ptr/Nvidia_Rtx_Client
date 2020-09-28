3992c811702dd5a231105d7b54c47dfa11d9038c
pozm <44528100+pozm@users.noreply.github.com>
2020-09-29 00:55:05 +0100

    loop argument.

--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@ import { Client } from "./classes.ts";
 import { localeMap } from "./globalData.ts";
 
 let Locale = (Deno.args[0] ?? "en-gb") as  "en-gb" | "en-us" // from argument 1
-let Dev = Deno.args[1] == "true" // from argument 2
+let Dev = Deno.args.includes("--dev")
+let Loop = Deno.args.includes("--loop")
 
 if (!Object.keys(localeMap).includes(Locale))
 {
@@ -12,4 +13,8 @@ if (!Object.keys(localeMap).includes(Locale))
 
 let rtxClient = new Client({Locale, Dev})
 
+
 await rtxClient.Check()
+if (!Loop)
+    Deno.exit(1)
+else setInterval(rtxClient.Check.bind(rtxClient),10e3)
