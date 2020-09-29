956653fb3ba81a997bb3e755d6cd8af8762cf5a0
brad <44528100+pozm@users.noreply.github.com>
2020-09-29 13:09:04 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ let rtxClient = new Client(
     { 
         Locale, 
         Dev,
-        WebhookUrl : "https://canary.discordapp.com/api/webhooks/760208968772616266/jUwz7LpPD3Q2tfQ22Yl30K14x4OJweBcKWMJEDK2Zk7maxMf9DV5BhGvtg3bpSeFmFAu"
+        WebhookUrl : ""
     }
 );
 await rtxClient.Check();
